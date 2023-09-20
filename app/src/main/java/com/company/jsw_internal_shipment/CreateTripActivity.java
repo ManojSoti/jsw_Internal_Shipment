22cdb5c755243d15e331fed2bfdc1a248c7c0c7b
MMithun <mithun.m@checkpt.com>
2023-09-20 11:38:40 +0530

    Mithun's commit

---
+++ app/src/main/java/com/company/jsw_internal_shipment/CreateTripActivity.java
@@ -0,0 +1,14 @@
+package com.company.jsw_internal_shipment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateTripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_trip);
+    }
+}
